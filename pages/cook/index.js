dc15014a31906cebebae8228b4c07bef05e22f14
zhuzhfien <zhifen@suncard.vip>
2020-06-02 14:48:31 +0800

    feat: 完善冲煮方法页面

--- pages/cook/index.js
+++ pages/cook/index.js
@@ -4,11 +4,20 @@ const app = getApp()
 
 Page({
   data: {
-    cook: coffeesData.cook
+    onShowMenu: 0,  //当前导航的下标
+    navMenu: coffeesData.cook,   // 一级导航
   },
 
   onLoad: function () {
-
+    wx.setNavigationBarTitle({
+      title: '冲煮方法'
+    })
   },
 
+  changeMenu (e) {
+    let idx = e.currentTarget.dataset.idx || 0
+    this.setData({
+      onShowMenu: idx,
+    })
+  }
 })
